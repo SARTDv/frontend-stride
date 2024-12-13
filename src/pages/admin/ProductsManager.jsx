31ab52ce68ee594a16461ab1a10ec1438e696e15
SARTDv <munozalejandro531@gmail.com>
2024-12-13 12:28:31 -0500

    Despliegue

---
+++ src/pages/admin/ProductsManager.jsx
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { ProductForm } from './ProductForm';
+import { ProductList } from './ProductList';
+import styles from '../../css/admin.module.css';
+
+export function ProductsManager() {
+  const [products, setProducts] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [editingProduct, setEditingProduct] = useState(null);
+
+  useEffect(() => {
+    // Obtener productos al cargar el componente
+    axios.get('http://127.0.0.1:8000/api/products/admin/products/')
+      .then(response => {
+        setProducts(response.data);
+      })
+      .catch(error => {
+        console.error("There was an error fetching the products!", error);
+      });
+  }, []);
+
+  const handleSaveProduct = (product) => {
+    if (editingProduct) {
+      // Actualizar el producto
+      axios.put(`http://127.0.0.1:8000/api/products/admin/products/${product.id}/`, product)
+        .then(response => {
+          setProducts(products.map(p => p.id === product.id ? response.data : p));
+          setShowForm(false);
+          setEditingProduct(null);
+        })
+        .catch(error => {
+          console.error("There was an error updating the product!", error);
+        });
+    } else {
+      // Crear un nuevo producto
+      axios.post('http://127.0.0.1:8000/api/products/admin/products/', product)
+        .then(response => {
+          setProducts([...products, response.data]);
+          setShowForm(false);
+        })
+        .catch(error => {
+          console.error("There was an error creating the product!", error);
+        });
+    }
+  };
+
+  const handleEdit = (product) => {
+    setEditingProduct(product);
+    console.log(product.imageurl)
+    setShowForm(true);
+  };
+
+  const handleDelete = (productId) => {
+    axios.delete(`http://127.0.0.1:8000/api/products/admin/products/${productId}/`)
+      .then(() => {
+        setProducts(products.filter(p => p.id !== productId));
+      })
+      .catch(error => {
+        console.error("There was an error deleting the product!", error);
+      });
+  };
+
+  return (
+    <div className={styles["products-manager"]}>
+      <div className={styles["header-actions"]}>
+        <h2>Product Management</h2>
+        <button 
+          className={`${styles.btn} ${styles["btn-primary"]}`}
+          onClick={() => {
+            setEditingProduct(null);
+            setShowForm(true);
+          }}
+        >
+          Add New Product
+        </button>
+      </div>
+
+      {showForm ? (
+        <ProductForm 
+          product={editingProduct}
+          onSave={handleSaveProduct}
+          onCancel={() => {
+            setShowForm(false);
+            setEditingProduct(null);
+          }}
+        />
+      ) : (
+        <ProductList 
+          products={products}
+          onEdit={handleEdit}
+          onDelete={handleDelete}
+        />
+      )}
+    </div>
+  );
+}
---
+++ src/pages/orders.jsx
@@ -0,0 +1,156 @@
+import React, { useState, useEffect } from 'react';
+import { Package, CheckCircle, CircleX } from 'lucide-react';
+import axios from 'axios';
+import styles from '../css/orders.module.css'; // Importar el archivo CSS modular
+
+const OrderPage = () => {
+  const [orders, setOrders] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // Cargar las órdenes desde el backend
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/orders/orders/');
+        setOrders(response.data);
+      } catch (err) {
+        setError(err.response ? err.response.data.detail : 'Failed to fetch orders');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchOrders();
+  }, []);
+
+  // Marcar como entregada
+  const handleMarkAsDelivered = async (orderId) => {
+    try {
+      const response = await axios.patch(`http://localhost:8000/api/orders/orders/${orderId}/`, {
+        status: 'completed',
+      });
+      // Actualiza el estado local de la orden
+      setOrders(
+        orders.map((order) =>
+          order.id === orderId ? { ...order, status: 'completed' } : order
+        )
+      );
+    } catch (err) {
+      setError(err.response ? err.response.data.detail : 'Failed to update order status');
+    }
+  };
+  
+  const handleMarkAsCanceled = async (orderId) => {
+    try {
+      const response = await axios.patch(`http://localhost:8000/api/orders/orders/${orderId}/`, {
+        status: 'cancelled',
+      });
+      // Actualiza el estado local de la orden
+      setOrders(
+        orders.map((order) =>
+          order.id === orderId ? { ...order, status: 'cancelled' } : order
+        )
+      );
+    } catch (err) {
+      setError(err.response ? err.response.data.detail : 'Failed to update order status');
+    }
+  };
+  if (loading) {
+    return <div>Loading orders...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div className={styles["orders-container"]}>
+      <div className={styles["orders-content"]}>
+        <div className={styles["orders-header"]}>
+          <Package size={32} className={styles["orders-icon"]} />
+          <h1 className={styles["orders-title"]}>My Orders</h1>
+        </div>
+
+        <div className={styles["orders-list"]}>
+          {orders.map((order) => (
+            <div key={order.id} className={styles["order-card"]}>
+              <div className={styles["order-content"]}>
+                <div className={styles["order-image-container"]}>
+                  <img
+                    src={order.FirstProductImageUrl}
+                    alt=""
+                    className={styles["order-image"]}
+                  />
+                </div>
+
+                <div className={styles["order-details"]}>
+                  <div className={styles["order-title"]}>
+                    <h3 className={styles["order-name"]}>
+                      Order Date: {new Date(order.orderDate).toLocaleDateString()}
+                    </h3>
+                  </div>
+
+                  <div className={styles["order-info"]}>
+                    <div className={styles["order-date"]}>
+                      <Package size={18} />
+                      <span>Package quantity: {order.packageQty}</span>
+                    </div>
+
+                    <div className={styles["order-status"]}>
+                      <span className={styles["status-label"]}>Status:</span>
+                      <span
+                        className={`${styles["status-value"]} ${styles[order.status]}`}
+                      >
+                        {order.status.charAt(0).toUpperCase() + order.status.slice(1)}
+                      </span>
+                    </div>
+                  </div>
+
+                  <div className={styles["order-id"]}>
+                    <span>Order Id: {order.id}</span>
+                  </div>
+
+                  <div className={styles["order-actions"]}>
+                    <div>
+                      <span className={styles["order-price"]}>
+                        Total amount: ${order.Totalprice}
+                      </span>
+                    </div>
+                    <div className={styles["right-divs"]}>
+                      {order.status === 'shipped' && (
+                        <>
+                          <button
+                            onClick={() => handleMarkAsDelivered(order.id)}
+                            className={styles["mark-received-btn"]}
+                          >
+                            <CheckCircle size={18} />
+                            Mark as Received
+                          </button>
+
+                          <button
+                            onClick={() => handleMarkAsCanceled(order.id)}
+                            className={styles["mark-canceled-btn"]}
+                          >
+                            <CircleX size={18} />
+                            Cancel this Order
+                          </button>
+                        </>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+
+
+      
+    </div>
+  );
+};
+
+export default OrderPage;
